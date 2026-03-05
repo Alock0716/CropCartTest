a5b4a3e7259ccf777914feb5dd171235f554fd94
Alock0716 <lockhaam2023@mountunion.edu>
2026-03-05 00:05:11 -0500

    Update farmer.js

--- scripts/farmer.js
+++ scripts/farmer.js
@@ -464,7 +464,7 @@
    * If none of these exist, you need to confirm the backend route name.
    */
   async function getStripeLinkOutUrl() {
-    const url = `${ROOT_BASE}/farmer/stripe/onboarding/`;
+    const url = `${ROOT_BASE}/farmer/stripe/account/`;
 
     // Most APIs generate a fresh account link with POST
     const res = await fetch(url, {
